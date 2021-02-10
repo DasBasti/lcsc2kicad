a9e93be992da1f78b0168d511e08e1e6dc13df4e
Bastian Neumann <bneumann@kurzschluss-engineering.de>
2021-02-10 21:29:38 +0100

    do not generate empty libraries

--- generate.py
+++ generate.py
@@ -26,26 +26,29 @@ table.load_data("uploadComponentInfo")
 
 parts = 0
 for lib in table.data:
+    lib_parts = 0
     print("Library: "+lib)
     file_pre = (lib.replace(" ","_").replace("&","").replace("__","_"))
     dcm_str = "EESchema-DOCLIB  Version 2.0\n"
     lib_str = "EESchema-LIBRARY Version 2.4\n"
-    lib_str +="#encoding utf-8\n"
+    lib_str += "#encoding utf-8\n"
     for grp in table.data[lib]:
         print("Group: "+grp)
         for part in table.data[lib][grp]:
             template = table.sanitise_name(part['SecondCategory'], spaces=False)
             if template in lib_templates:
-                parts+=1
+                parts += 1
+                lib_parts += 1
                 lib_str += lib_templates[template].format(**part)
                 dcm_str += dcm_template.format(**part)
 
     lib_str += "#\n#End Library"
     dcm_str += "#\n#End Doc Library"
 
-    with open("export/LCSC_"+file_pre+".lib", "w") as l:
-        l.write(lib_str)
-    with open("export/LCSC_"+file_pre+".dcm", "w") as l:
-        l.write(dcm_str)
+    if(lib_parts > 0):
+        with open("export/LCSC_"+file_pre+".lib", "w") as l:
+            l.write(lib_str)
+        with open("export/LCSC_"+file_pre+".dcm", "w") as l:
+            l.write(dcm_str)
 
 print("Total: {0} parts".format(parts))
