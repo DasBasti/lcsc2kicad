fb916544d450e3ffdabd59bdc3bbdd9f6c204300
Bastian Neumann <bneumann@kurzschluss-engineering.de>
2021-02-12 16:12:51 +0100

    Optimize Library generation
    Templates now get a header JSON string in the first line.
    It is marked with #! and is left out from the library file
    For now only the number of Pins is in the header and
    needs to fit the parts solder joint field.
    Preperations are put into the generator function to
    have part specific templates and the group template as
    fall back if a part tempate is not found.
    
    Template files are updated to contain pins header information.

--- generate.py
+++ generate.py
@@ -1,7 +1,9 @@
 import table
 import os
+import sys
 import glob
 import requests
+import json
 from xlrd import open_workbook
 
 dcm_templates = {}
@@ -17,15 +19,28 @@ with open("templates/template.dcm") as f:
 for filepath in glob.iglob("templates/*.lib"):
     print("load: "+filepath)
     with open(filepath) as f:
-        lib_templates[os.path.basename(filepath).split('.')[0]] = f.read()
+        content = f.read()
+        lines = content.splitlines()
+        header = {}
+        if lines[0].startswith("#!"): # parse header information
+            header = json.loads(lines[0][2:])
+        header['content'] = "\n".join(lines[1:])
+        lib_templates[os.path.basename(filepath).split('.')[0]] = header
 
-print("fetch file from JLCPCB")
-url = 'https://jlcpcb.com/componentSearch/uploadComponentInfo'
-r = requests.get(url)
+if len(sys.argv) == 1:
+    print("fetch file from JLCPCB")
+    url = 'https://jlcpcb.com/componentSearch/uploadComponentInfo'
+    r = requests.get(url)
+    db = r.content
+else:
+    print("load from: ", sys.argv[1])
+    with open(sys.argv[1], "rb") as db_file:
+        db = db_file.read()
+
 
 print("parsing data. This might take a while!")
 
-table.load_data(r.content)
+table.load_data(db)
 
 parts = 0
 for lib in table.data:
@@ -38,12 +53,26 @@ for lib in table.data:
     for grp in table.data[lib]:
         print("Group: "+grp)
         for part in table.data[lib][grp]:
-            template = table.sanitise_name(part['SecondCategory'], spaces=False)
+            # Test for MPN Template
+            template = table.sanitise_name(part['MFRPart'], spaces=False)
             if template in lib_templates:
-                parts += 1
-                lib_parts += 1
-                lib_str += lib_templates[template].format(**part)
-                dcm_str += dcm_template.format(**part)
+                if lib_templates[template]['pins'] == part['SolderJoint']:
+                    parts += 1
+                    lib_parts += 1
+                    lib_str += lib_templates[template]['content'].format(**part)
+                    dcm_str += dcm_template.format(**part)
+                else:
+                    print("Pins missmatch", part['SolderJoint'], template)
+            else: # Test for generic Group Template
+                template = table.sanitise_name(part['SecondCategory'], spaces=False)
+                if template in lib_templates:
+                    if lib_templates[template]['pins'] == part['SolderJoint']:
+                        parts += 1
+                        lib_parts += 1
+                        lib_str += lib_templates[template]['content'].format(**part)
+                        dcm_str += dcm_template.format(**part)
+                    else:
+                        print("Group Pins missmatch", part['SolderJoint'], part['MFRPart'])
 
     lib_str += "#\n#End Library"
     dcm_str += "#\n#End Doc Library"
